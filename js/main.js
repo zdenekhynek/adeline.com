362c7e8251f32055f482594dba9887f24fac7970
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-04-04 10:04:17 +0100

    prototype layout - resets scroll

--- js/main.js
+++ js/main.js
@@ -6,14 +6,38 @@
 
 		FastClick.attach( document.body );
 
-		var $pageInnerWrap = $( ".page-inner-wrap" );
+		var $pageWrap = $( ".page-wrap" ),
+			$pageRightColumn = $pageWrap.find( ".page-right-column" ),
+			$navOpenBtn = $pageWrap.find( "#nav-open-btn" );
 
-		$( "#nav-open-btn" ).on( "click", function( evt ) {
+		var togglePageWrapOpenClass = function( evt ) {
 
-			evt.preventDefault();
-			$pageInnerWrap.toggleClass( "open" );
-		
-		} );
+			evt.stopImmediatePropagation();
+
+			//freeze scroll
+			if( !$pageWrap.hasClass( "open" ) ) {
+				console.log( "freezing scroll" );
+				$pageRightColumn.scrollTop( $( "body" ).scrollTop() );
+			} else {
+				
+			}
+			
+
+
+			$pageWrap.toggleClass( "open" );
+			
+			//add additional click listener to right column for easier closing			
+			if( $pageWrap.hasClass( "open" ) ) {
+				$pageRightColumn.on( "click", togglePageWrapOpenClass );
+			} else {
+				$pageRightColumn.off( "click" );// togglePageWrapOpenClass );
+			}
+
+		};
+
+		$navOpenBtn.on( "click", togglePageWrapOpenClass );
+
+
 
 	} ); 
 
